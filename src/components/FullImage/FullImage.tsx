227f504ce5947035c0c969cf1352a95432f98c02
yneli <Mr.RadReIII@yandex.ru>
2023-02-27 18:15:54 +0300

    img fix

--- src/components/FullImage/FullImage.tsx
+++ src/components/FullImage/FullImage.tsx
@@ -2,7 +2,7 @@ import style from "./FullImage.module.scss";
 export const FullImage = () => {
   return <div className={style.fullimage}>
     <div className={style.img}>
-        <img src="../assets/imgfull.png" alt="" />
+      <img src="../assets/imgfull.png" alt="" />
     </div>
 </div>;
 };
